f34c2bc7b7eb832cf19f3660eba00bf1ad882d23
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-12 18:04:12 -0300

    Lesson 04: Object toString

---
+++ 03-Object-e-toString/src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+  public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Product product = new Product();
+
+    System.out.println("Enter product data: ");
+
+    System.out.print("Name: ");
+    product.name = sc.next();
+    System.out.print("Price: ");
+    product.price = sc.nextDouble();
+    System.out.print("Quantity in stock: ");
+    product.quantity = sc.nextInt();
+
+    System.out.println(product);
+
+        /*
+        System.out.printf("Product data: %s, $ %.2f, %d units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        product.addProduct(sc.nextInt(product.quantity));
+
+        System.out.printf("Updated data: %s, $ %.2f, %d units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
+
+        System.out.print("Enter the number of products to be remove from stock: ");
+        product.removeProduct(sc.nextInt(product.quantity));
+
+        System.out.printf("Updated data: %s, $ %.2f, %d units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
+        */
+
+    sc.close();
+  }
+}
